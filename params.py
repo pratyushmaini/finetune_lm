6f5e23cce4dad4fecdffc0271983f33c538e2470
Pratyush Maini <pratyus2@fac-lipton03.tepper.cmu.edu>
2023-11-14 06:37:43 -0500

    updating default warmup

--- params.py
+++ params.py
@@ -9,7 +9,7 @@ def parse_args():
     parser.add_argument('--per_device_train_batch_size', type=int, default=4)
     parser.add_argument('--num_epochs', type=int, default=1)
     parser.add_argument('--num_save_steps', type=int, default=5)
-    parser.add_argument('--warmup_ratio', type=float, default=0.05)
+    parser.add_argument('--warmup_ratio', type=float, default=0.01)
     parser.add_argument('--weight_decay', type=float, default=1e-4)
     parser.add_argument('--logging_steps', type=int, default=10)
     parser.add_argument('--save_strategy', type=str, default='steps')
